fde69b1ba2b4e891c36d025a895eb1ab97e2a44e
aminosan <amino@the-menz.com>
2018-03-10 13:29:17 +0000

    AIファイルアップロードとサムネ化

--- app/uploaders/design_file_uploader.rb
+++ app/uploaders/design_file_uploader.rb
@@ -1,6 +1,6 @@
 class DesignFileUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -29,19 +29,37 @@ class DesignFileUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process :convert => 'png'
+    process :resize_to_limit => [512, 512]
+    def full_filename (for_file = model.artifact.file)
+      super.chomp(File.extname(super)) + '.png'
+    end
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg png ai)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
+  def filename
+    "#{secure_token(10)}.#{file.extension}" if original_filename.present?
+  end
+
+  before :cache, :save_original_filename
+  def save_original_filename(file)
+    model.design_original_filename ||= file.original_filename if file.respond_to?(:original_filename)
+  end
+
+  protected
+    def secure_token(length=16)
+      var = :"@#{mounted_as}_secure_token"
+      model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
+    end
 end
